2853c273f7cf1080eb043901fe97af77a23baf99
Maxorq <psqmaxor@gmail.com>
2013-12-21 18:33:18 +0100

    Adding Redis; changed some !mikuia commands to admin only

--- plugins/base.js
+++ plugins/base.js
@@ -6,9 +6,10 @@ exports.commands = ['mikuia']
 exports.handleCommand = function(tokens, from, channel) {
 	switch(tokens[0]) {
 		case 'mikuia':
-			if(from == Mikuia.settings.plugins.base.admin.toLowerCase()) {
-				switch(tokens[1]) {
-					case 'channels':
+			//if(from == Mikuia.settings.plugins.base.admin.toLowerCase()) {
+			switch(tokens[1]) {
+				case 'channels':
+					if(from == Mikuia.settings.plugins.base.admin.toLowerCase()) {
 						var channelList = ""
 						_.each(Mikuia.channels, function(channel, channelName) {
 							if(channelList != "") {
@@ -17,11 +18,13 @@ exports.handleCommand = function(tokens, from, channel) {
 							channelList += channelName
 						})
 						Mikuia.say(channel, 'Enabled channels: ' + channelList)
-						break
-					case 'donate':
-						Mikuia.say(channel, 'Aww, you want to donate? Thank you! http://bit.ly/1clkjef')
-						break
-					case 'plugins':
+					}
+					break
+				case 'donate':
+					Mikuia.say(channel, 'Aww, you want to donate? Thank you! http://bit.ly/1clkjef')
+					break
+				case 'plugins':
+					if(from == Mikuia.settings.plugins.base.admin.toLowerCase()) {
 						var pluginList = ""
 						_.each(Mikuia.plugins, function(plugin, pluginName) {
 							if(pluginList != "") {
@@ -30,12 +33,13 @@ exports.handleCommand = function(tokens, from, channel) {
 							pluginList += pluginName
 						})
 						Mikuia.say(channel, 'Loaded plugins: ' + pluginList)
-						break
-					default:
-						Mikuia.say(channel, 'Hey, I\'m Mikuia, and I\'m a bot! Learn more about me at http://statpoint.info:5587/')
-						break
-				}
+					}
+					break
+				default:
+					Mikuia.say(channel, 'Hey, I\'m Mikuia, and I\'m a bot! Learn more about me at http://statpoint.info:5587/')
+					break
 			}
+			//}
 			break
 	}
 }
