7be736ecc0bf998d16375261ace01199b032cb43
Maxorq <psqmaxor@gmail.com>
2014-01-22 23:57:26 +0100

    Select the hardest difficulty when requesting set

--- osuapi.js
+++ osuapi.js
@@ -8,8 +8,17 @@ var osu = function(key) {
 osu.prototype.getBeatmap = function(beatmapId, type, callback) {
 	request('http://osu.ppy.sh/api/get_beatmaps?k=' + this.key + '&' + type + '=' + beatmapId, function(error, response, body) {
 		if(!error && body != '[]') {
-			var json = JSON.parse(body)[0]
-			callback(false, json)
+			var json = JSON.parse(body)
+			var maxRating = 0
+			var select = 0
+			_.each(json, function(value, key, list) {
+				if(value.difficultyrating > maxRating && value.mode == 0) {
+					maxRating = value.difficultyrating
+					select = key
+				}
+			})
+			//console.log(json)
+			callback(false, json[select])
 		} else {
 			callback(true, {})
 		}
